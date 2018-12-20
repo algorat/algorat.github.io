e8c2591c2993045defdd7084616252366891e769
algorat <45745900+algorat@users.noreply.github.com>
2018-12-20 01:59:41 -0500

    randomize rat on click

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 window.onload = () => {
+
+  var rats = ['img/rat3.png', 'img/rat4.png', 'img/rat5.png']; 
+
   window.addEventListener('click', e => {
     let rat = document.createElement('img');
-    rat.src = 'img/rat.png';
+    rat.src = rats[(Math.random() * rats.length) | 0];
     rat.style.position = 'absolute';
-    rat.style.left = e.pageX - 20;
-    rat.style.top = e.pageY - 20;
-    rat.style.width = '40px';
+    rat.style.left = e.pageX - 50;
+    rat.style.top = e.pageY - 50;
+    rat.style.width = '100px';
     document.body.append(rat);
   });
 };
