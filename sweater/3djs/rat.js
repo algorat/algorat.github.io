2946262678de449ab7eb9fe2f3e31a9025ec798f
carolinehermans <carooolinehermans@gmail.com>
2019-12-23 00:57:56 -0500

    update UI for sweateratreator, background changing not hooked up yet

--- sweater/3djs/rat.js
+++ sweater/3djs/rat.js
@@ -1,4 +1,3 @@
-
 import * as THREE from './three.js-master/build/three.module.js';
 import Stats from './three.js-master/examples/jsm/libs/stats.module.js';
 import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js';
@@ -6,161 +5,170 @@ import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitCont
 import { FBXLoader } from './three.js-master/examples/jsm/loaders/FBXLoader.js';
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 50 );
+var camera = new THREE.PerspectiveCamera(75, 1, 0.1, 50);
 //var camera = new THREE.OrthographicCamera(-300,300,-300,300, 150, 1000);
 //
 camera.position.z = 3;
 camera.position.y = 3;
-var renderer = new THREE.WebGLRenderer( { antialias: true, canvas: document.getElementById("gl-canvas") } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( 512, 512 );
-renderer.setClearColor( 0x000000, 1 );
+var renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas: document.getElementById('gl-canvas')
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(512, 512);
+renderer.setClearColor(0x000000, 1);
 
-var orbit = new OrbitControls( camera, renderer.domElement );
+var orbit = new OrbitControls(camera, renderer.domElement);
 
 var lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xffff00, 1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xff00ff, 1, 0 );
-lights[ 2 ] = new THREE.PointLight( 0x00ffff, 1, 0 );
+lights[0] = new THREE.PointLight(0xffff00, 1, 0);
+lights[1] = new THREE.PointLight(0xff00ff, 1, 0);
+lights[2] = new THREE.PointLight(0x00ffff, 1, 0);
 
-lights[ 0 ].position.set( 0, 200, 0 );
-lights[ 1 ].position.set( 100, 200, 100 );
-lights[ 2 ].position.set( - 100, - 200, - 100 );
+lights[0].position.set(0, 200, 0);
+lights[1].position.set(100, 200, 100);
+lights[2].position.set(-100, -200, -100);
 
-scene.add( lights[ 0 ] );
-scene.add( lights[ 1 ] );
-scene.add( lights[ 2 ] );
+scene.add(lights[0]);
+scene.add(lights[1]);
+scene.add(lights[2]);
 
 var group = new THREE.Group();
 
 var geometry = new THREE.BufferGeometry();
-geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [], 3 ) );
+geometry.addAttribute('position', new THREE.Float32BufferAttribute([], 3));
 
-var lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff, transparent: true, opacity: 0.5 } );
-var meshMaterial = new THREE.MeshPhongMaterial( { color: 0xffff00, emissive: 0xccce00, side: THREE.DoubleSide, flatShading: true } );
+var lineMaterial = new THREE.LineBasicMaterial({
+  color: 0xffffff,
+  transparent: true,
+  opacity: 0.5
+});
+var meshMaterial = new THREE.MeshPhongMaterial({
+  color: 0xffff00,
+  emissive: 0xccce00,
+  side: THREE.DoubleSide,
+  flatShading: true
+});
 
-group.add( new THREE.LineSegments( geometry, lineMaterial ) );
-group.add( new THREE.Mesh( geometry, meshMaterial ) );
-var loadedRat = false
-var texture
-function loadRat(){
-    let ratvas = document.getElementById('ratvas') // get the canvas and connect to texture 
-    let ctx = ratvas.getContext('2d')
-var backgrounds = ['winter_landscape.jpg',
-        'snowflakes.jpg',
-        'leaves.jpg',
-        'hearth.jpg']
-var randomNumber = Math.floor(Math.random()*backgrounds.length);
-var backgroundName = backgrounds[randomNumber];
+group.add(new THREE.LineSegments(geometry, lineMaterial));
+group.add(new THREE.Mesh(geometry, meshMaterial));
+var loadedRat = false;
+var texture;
+function loadRat() {
+  let ratvas = document.getElementById('ratvas'); // get the canvas and connect to texture
+  let ctx = ratvas.getContext('2d');
+  var backgrounds = [
+    'winter_landscape.jpg',
+    'snowflakes.jpg',
+    'leaves.jpg',
+    'hearth.jpg'
+  ];
+  var randomNumber = Math.floor(Math.random() * backgrounds.length);
+  var backgroundName = backgrounds[randomNumber];
 
-// scene.background = new THREE.CubeTextureLoader()
-//  .setPath( 'assets/backgrounds/' )
-//  .load( [
-//      backgroundName,
-//      backgroundName,
-//      backgroundName,
-//      backgroundName,
-//      backgroundName,
-//      backgroundName,
+  // scene.background = new THREE.CubeTextureLoader()
+  //  .setPath( 'assets/backgrounds/' )
+  //  .load( [
+  //      backgroundName,
+  //      backgroundName,
+  //      backgroundName,
+  //      backgroundName,
+  //      backgroundName,
+  //      backgroundName,
 
-//  ] );
+  //  ] );
 
- //Load background texture
-const loaderImg = new THREE.TextureLoader();
-loaderImg.load('assets/backgrounds/' + backgroundName , function(bg)
-            {
-             scene.background = bg;  
-            });
+  //Load background texture
+  const loaderImg = new THREE.TextureLoader();
+  loaderImg.load('assets/backgrounds/' + backgroundName, function(bg) {
+    scene.background = bg;
+  });
 
-    texture = new THREE.CanvasTexture(ctx.canvas);//,THREE.UVMapping,THREE.RepeatWrapping,THREE.RepeatWrapping);
-    const material = new THREE.MeshBasicMaterial({
-      map: texture,
-    });
-    texture.needsUpdate = true
-    var loader = new FBXLoader();
+  texture = new THREE.CanvasTexture(ctx.canvas); //,THREE.UVMapping,THREE.RepeatWrapping,THREE.RepeatWrapping);
+  const material = new THREE.MeshBasicMaterial({
+    map: texture
+  });
+  texture.needsUpdate = true;
+  var loader = new FBXLoader();
 
-    loader.load( './3dAssets/sweater2.fbx', function ( object ) {
+  loader.load('./3dAssets/sweater2.fbx', function(object) {
     // mixer = new THREE.AnimationMixer( object );
     // var action = mixer.clipAction( object.animations[ 0 ] );
     // action.play();
-    	object.traverse( function ( child ) {
-    	if ( child.isMesh ) {
+    object.traverse(function(child) {
+      if (child.isMesh) {
+        child.material = new THREE.MeshBasicMaterial({
+          map: texture,
+          reflectivity: 0.0,
+          shininess: 0.0,
+          bumpScale: 1.0
+        });
+        child.geometry.uvsNeedUpdate = true;
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    group.add(object);
+  });
 
-    		child.material = new THREE.MeshBasicMaterial({
-    		  map: texture,
-                reflectivity: 0.0,
-                shininess: 0.0,
-                bumpScale: 1.0
-    		});
-            child.geometry.uvsNeedUpdate = true;
-            child.castShadow = true;
-            child.receiveShadow = true;
-    	}
-    	} );
-    	group.add( object );
-    } );
+  loader.load('./3dAssets/rat.fbx', function(object) {
+    object.traverse(function(child) {
+      if (child.isMesh) {
+        var diffuseColor = new THREE.Color().setHSL(Math.random(), 0.2, 0.2);
 
-    loader.load( './3dAssets/rat.fbx', function ( object ) {
-
-    	object.traverse( function ( child ) {
-    	if ( child.isMesh ) {
-            var diffuseColor = new THREE.Color().setHSL( Math.random(),0.2,0.2);
-
-            child.material = new THREE.MeshToonMaterial( {
-                                reflectivity: 1.0,
-                                shininess: 1.0,
-                                bumpScale: 1.0,
-                                color: diffuseColor
-                            } );
-    		child.castShadow = true;
-    		child.receiveShadow = true;
-    	}
-    	} );
-    	group.add( object );
-    } );
-
+        child.material = new THREE.MeshToonMaterial({
+          reflectivity: 1.0,
+          shininess: 1.0,
+          bumpScale: 1.0,
+          color: diffuseColor
+        });
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    group.add(object);
+  });
 }
 //var options = chooseFromHash( group );
 
-
-
 // scene.background.warpS = scene.background.warpT = THREE.RepeatWrapping;
-// scene.background.repeat.set(30,30); 
+// scene.background.repeat.set(30,30);
 group.position.y -= 0.8;
-scene.add( group );
+scene.add(group);
 
 var prevFog = false;
 
-var render = function () {
-    // we need all the p5 code in canvas.js to run before we can grab the canvas
-    if (loadedRat == false && document.getElementById('ratvas') != null){
-        loadRat()
-        loadedRat = true
-    }
-    if (loadedRat == true){
-        texture.needsUpdate = true
-    }
-	requestAnimationFrame( render );
+var render = function() {
+  // we need all the p5 code in canvas.js to run before we can grab the canvas
+  if (loadedRat == false && document.getElementById('ratvas') != null) {
+    loadRat();
+    loadedRat = true;
+  }
+  if (loadedRat == true) {
+    texture.needsUpdate = true;
+  }
+  requestAnimationFrame(render);
 
-	//if ( ! options.fixed ) {
+  //if ( ! options.fixed ) {
 
-		group.rotation.x += Math.sin(group.rotation.y* 10)* 0.001;
-        group.position.y += Math.sin(group.rotation.y* 5)* 0.001;
-		group.rotation.y += 0.001;
+  group.rotation.x += Math.sin(group.rotation.y * 10) * 0.001;
+  group.position.y += Math.sin(group.rotation.y * 5) * 0.001;
+  group.rotation.y += 0.001;
 
-	//}
+  //}
 
-	renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 };
 
-window.addEventListener( 'resize', function () {
+window.addEventListener(
+  'resize',
+  function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
-	//renderer.setSize( window.innerWidth, window.innerHeight );
-
-}, false );
+    //renderer.setSize( window.innerWidth, window.innerHeight );
+  },
+  false
+);
 
 render();
--- sweater/js/canvas.js
+++ sweater/js/canvas.js
@@ -5,47 +5,46 @@
 //TOOD: SET CURRENT SLIDER VAL NEXT TO SLIDER
 
 class HistoryItem {
-    constructor(startX, startY, endX, endY, color, weight) {
-        this.startX = startX;
-        this.startY = startY;
-        this.endX = endX;
-        this.endY = endY;
-        this.color = color;
-        this.weight = weight;
-    }
-    draw() {
-        fill(this.color);
-        stroke(this.color);
-        strokeWeight(this.weight);
-        line(this.startX, this.startY, this.endX, this.endY);
-    }
+  constructor(startX, startY, endX, endY, color, weight) {
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+    this.color = color;
+    this.weight = weight;
+  }
+  draw() {
+    fill(this.color);
+    stroke(this.color);
+    strokeWeight(this.weight);
+    line(this.startX, this.startY, this.endX, this.endY);
+  }
 }
 
-
 var darkColors = [
-    "#D83D3D",
-    "#DD9B7E",
-    "#9B7048",
-    "#80793E",
-    "#4A5A72",
-    "#444EA4",
-    "#9F7CA5",
-    "#C28D8D",
-    "#818181",
-    "#000000"
+  '#D83D3D',
+  '#DD9B7E',
+  '#9B7048',
+  '#80793E',
+  '#4A5A72',
+  '#444EA4',
+  '#9F7CA5',
+  '#C28D8D',
+  '#818181',
+  '#000000'
 ];
 
 var lightColors = [
-    "#F8D9D9",
-    "#EBBF96",
-    "#FFC266",
-    "#C7E7AF",
-    "#A6D5D5",
-    "#9FC6FF",
-    "#DDC2FF",
-    "#FFD3D3",
-    "#DBDBDB",
-    "#FFFFFF"
+  '#F8D9D9',
+  '#EBBF96',
+  '#FFC266',
+  '#C7E7AF',
+  '#A6D5D5',
+  '#9FC6FF',
+  '#DDC2FF',
+  '#FFD3D3',
+  '#DBDBDB',
+  '#FFFFFF'
 ];
 
 //history of all strokes
@@ -72,192 +71,209 @@ var prevY = null;
 var backgroundMode = false;
 
 function setup() {
-    let can = createCanvas(512, 512);
-    can.parent('ratvas-container');
-    can.id('ratvas');
-    document.onmouseup = released;
+  let can = createCanvas(512, 512);
+  can.parent('ratvas-container');
+  can.id('ratvas');
+  document.onmouseup = released;
 
-    currentWidth = startingWidth;
-    currentColor = color(startingColor);
-    currentBackgroundColor = color(startingBg);
-    
-    strokeCap('ROUND');
-    background(currentBackgroundColor);
-    strokeWeight(currentWidth);
-    fill(currentColor);
-    stroke(currentColor);
+  currentWidth = startingWidth;
+  currentColor = color(startingColor);
+  currentBackgroundColor = color(startingBg);
+
+  strokeCap('ROUND');
+  background(currentBackgroundColor);
+  strokeWeight(currentWidth);
+  fill(currentColor);
+  stroke(currentColor);
 }
 
 function mouseDragged() {
-    console.log("drag", prevX, prevY, mouseX, mouseY)
-    if (prevX == null && prevY == null) {
-        //if not off canvas
-        if(mouseX >= 0 && mouseY >= 0 && mouseX <= 500 && mouseY <= 500){
-            prevX = mouseX;
-            prevY = mouseY;
-        }
-    } else {
-        strokeWeight(currentWidth);
-        fill(currentColor);
-        stroke(currentColor);
-        line(prevX, prevY, mouseX, mouseY);
-
-        let historyItem = new HistoryItem(prevX, prevY, mouseX, mouseY, currentColor, currentWidth);
-        currentPathHistory.push(historyItem);
-
-        prevX = mouseX;
-        prevY = mouseY;
+  if (prevX == null && prevY == null) {
+    //if not off canvas
+    if (mouseX >= 0 && mouseY >= 0 && mouseX <= 500 && mouseY <= 500) {
+      prevX = mouseX;
+      prevY = mouseY;
     }
+  } else {
+    strokeWeight(currentWidth);
+    fill(currentColor);
+    stroke(currentColor);
+    line(prevX, prevY, mouseX, mouseY);
+
+    let historyItem = new HistoryItem(
+      prevX,
+      prevY,
+      mouseX,
+      mouseY,
+      currentColor,
+      currentWidth
+    );
+    currentPathHistory.push(historyItem);
+
+    prevX = mouseX;
+    prevY = mouseY;
+  }
 }
 
 function released() {
-    console.log("rel")
-    prevX = null;
-    prevY = null;
-    if (currentPathHistory.length > 0){
-        strokeHistory.push(currentPathHistory);
-        currentPathHistory = [];
-        undoneStrokes = [];
-    }
+  prevX = null;
+  prevY = null;
+  if (currentPathHistory.length > 0) {
+    strokeHistory.push(currentPathHistory);
+    currentPathHistory = [];
+    undoneStrokes = [];
+  }
 }
 
 function undo() {
-    if(strokeHistory.length < 1) return;
-    undoneStrokes.unshift(strokeHistory.pop());
-    redrawHistory();
+  if (strokeHistory.length < 1) return;
+  undoneStrokes.unshift(strokeHistory.pop());
+  redrawHistory();
 }
 
 function redo() {
-    if(undoneStrokes.length < 1) return;
-    strokeHistory.push(undoneStrokes.shift());
-    redrawHistory();
+  if (undoneStrokes.length < 1) return;
+  strokeHistory.push(undoneStrokes.shift());
+  redrawHistory();
 }
 
 function redrawHistory() {
-    background(currentBackgroundColor);
-    strokeHistory.forEach(path => path.forEach(ele => ele.draw()));
+  background(currentBackgroundColor);
+  strokeHistory.forEach(path => path.forEach(ele => ele.draw()));
 }
 
-function unattachColIndicator(ele){
-    var indicator = ele.querySelector("#col-indicator");
-    indicator.remove();
-    return indicator;
+function unattachColIndicator(ele) {
+  var indicator = ele.querySelector('#col-indicator');
+  indicator.remove();
+  return indicator;
 }
 
-function unattachBgIndicator(ele){
-    var indicator = ele.querySelector("#bg-indicator");
-    indicator.remove();
-    return indicator;
+function unattachBgIndicator(ele) {
+  var indicator = ele.querySelector('#bg-indicator');
+  indicator.remove();
+  return indicator;
 }
 
-function attachIndicator(ele, indicator){
-    console.log(ele, indicator);
-    ele.appendChild(indicator);
+function attachIndicator(ele, indicator) {
+  ele.appendChild(indicator);
 }
 
 function generateColorPalette(colorContainer) {
-    let rows = darkColors.length;
+  let rows = darkColors.length;
 
-    let colIndicator = document.createElement("IMG");
-    colIndicator.id = "col-indicator";
-    colIndicator.src = "assets/brush.svg";
+  let colIndicator = document.createElement('IMG');
+  colIndicator.id = 'col-indicator';
+  colIndicator.src = 'assets/brush.svg';
 
-    let bgIndicator = document.createElement("IMG");
-    bgIndicator.id = "bg-indicator";
-    bgIndicator.src = "assets/background.svg";
+  let bgIndicator = document.createElement('IMG');
+  bgIndicator.id = 'bg-indicator';
+  bgIndicator.src = 'assets/background.svg';
 
-    for (let i = 0; i < rows; i++) {
-        let darkColor = darkColors[i];
-        let lightColor = lightColors[i];
-        var rowElement = document.createElement("DIV");
-        rowElement.classList.add("colorpalette-row");
+  for (let i = 0; i < rows; i++) {
+    let darkColor = darkColors[i];
+    let lightColor = lightColors[i];
+    var rowElement = document.createElement('DIV');
+    rowElement.classList.add('colorpalette-row');
 
-        var darkColorElement = document.createElement("DIV");
-        var lightColorElement = document.createElement("DIV");
+    var darkColorElement = document.createElement('DIV');
+    var lightColorElement = document.createElement('DIV');
 
-        darkColorElement.classList.add("color-item");
-        darkColorElement.style.backgroundColor = darkColor;
+    darkColorElement.classList.add('color-item');
+    darkColorElement.style.backgroundColor = darkColor;
 
-        lightColorElement.classList.add("color-item");
-        lightColorElement.style.backgroundColor = lightColor;
-        if (lightColor === "#FFFFFF") {
-            lightColorElement.style.border = "2px solid rgba(0, 0, 0, 0.3)";
-        }
-
-        darkColorElement.addEventListener("click", function(){setColor(this, darkColor)});
-        lightColorElement.addEventListener("click", function(){setColor(this, lightColor)});
-
-        //need to keep track of currently selected color elements so we can attach the indicator to them
-        if(darkColor === startingColor){
-            prevColorElement = darkColorElement;
-            attachIndicator(prevColorElement, colIndicator);
-        } 
-        if(lightColor === startingBg){
-            prevBgElement = lightColorElement;
-            attachIndicator(prevBgElement, bgIndicator);
-        }
-
-        rowElement.appendChild(darkColorElement);
-        rowElement.appendChild(lightColorElement);
-
-        console.log(colorContainer)
-        colorContainer.appendChild(rowElement);
+    lightColorElement.classList.add('color-item');
+    lightColorElement.style.backgroundColor = lightColor;
+    if (lightColor === '#FFFFFF') {
+      lightColorElement.style.border = '2px solid rgba(0, 0, 0, 0.3)';
     }
+
+    darkColorElement.addEventListener('click', function() {
+      setColor(this, darkColor);
+    });
+    lightColorElement.addEventListener('click', function() {
+      setColor(this, lightColor);
+    });
+
+    //need to keep track of currently selected color elements so we can attach the indicator to them
+    if (darkColor === startingColor) {
+      prevColorElement = darkColorElement;
+      attachIndicator(prevColorElement, colIndicator);
+    }
+    if (lightColor === startingBg) {
+      prevBgElement = lightColorElement;
+      attachIndicator(prevBgElement, bgIndicator);
+    }
+
+    rowElement.appendChild(darkColorElement);
+    rowElement.appendChild(lightColorElement);
+
+    console.log(colorContainer);
+    colorContainer.appendChild(rowElement);
+  }
 }
 
 function setColor(element, col) {
-    if(backgroundMode){
-        currentBackgroundColor = color(col);
-        redrawHistory();
+  if (backgroundMode) {
+    currentBackgroundColor = color(col);
+    redrawHistory();
 
-        let indicator = unattachBgIndicator(prevBgElement);
-        attachIndicator(element, indicator);
-        prevBgElement = element;
+    let indicator = unattachBgIndicator(prevBgElement);
+    attachIndicator(element, indicator);
+    prevBgElement = element;
+  } else {
+    currentColor = color(col);
 
-    } else {
-        currentColor = color(col);
-
-        let indicator = unattachColIndicator(prevColorElement);
-        attachIndicator(element, indicator);
-        prevColorElement = element;
-    }
+    let indicator = unattachColIndicator(prevColorElement);
+    attachIndicator(element, indicator);
+    prevColorElement = element;
+  }
 }
 
 function setWidth(wid) {
-    console.log(wid);
-    currentWidth = Math.floor(parseFloat(wid));
+  currentWidth = Math.floor(parseFloat(wid));
 }
 
-function setBg(val){
-    backgroundMode = val;
+function setBg(val) {
+  backgroundMode = val;
 }
 
+var backgroundPreviews;
+function changeRatBackground(ele) {
+  for (let i = 0; i < backgroundPreviews.length; i++) {
+    let bg = backgroundPreviews[i];
+    if (bg.classList.contains('background-preview-active')) {
+      bg.classList.remove('background-preview-active');
+    }
+  }
+  ele.classList.add('background-preview-active');
+}
 
 /* set up DOM parts */
-window.onload = function () {
-    var colorContainer = document.getElementById("colorpalette");
-    generateColorPalette(colorContainer);
+window.onload = function() {
+  backgroundPreviews = document.getElementsByClassName('background-preview');
+  var colorContainer = document.getElementById('colorpalette');
+  generateColorPalette(colorContainer);
 
-    document.getElementById("undo").addEventListener("click", () => undo());
-    document.getElementById("redo").addEventListener("click", () => redo());
+  document.getElementById('undo').addEventListener('click', () => undo());
+  document.getElementById('redo').addEventListener('click', () => redo());
 
-    document.getElementById("widthSlider").addEventListener("input", function () { setWidth(this.value) });
-    document.getElementById("widthSlider").value = startingWidth;
+  document.getElementById('widthSlider').addEventListener('input', function() {
+    setWidth(this.value);
+  });
+  document.getElementById('widthSlider').value = startingWidth;
 
-    let bg = document.getElementById("background");
-    let brush = document.getElementById("brush");
+  let bg = document.getElementById('background');
+  let brush = document.getElementById('brush');
 
-    document.getElementById("brush").addEventListener("click", 
-        function(){
-            this.classList.remove("not-selected");
-            bg.classList.add("not-selected");
-            setBg(false);
-        });
+  document.getElementById('brush').addEventListener('click', function() {
+    this.classList.remove('not-selected');
+    bg.classList.add('not-selected');
+    setBg(false);
+  });
 
-    document.getElementById("background").addEventListener("click", 
-        function(){ 
-            this.classList.remove("not-selected");
-            brush.classList.add("not-selected");
-            setBg(true); 
-        });
-}
+  document.getElementById('background').addEventListener('click', function() {
+    this.classList.remove('not-selected');
+    brush.classList.add('not-selected');
+    setBg(true);
+  });
+};
